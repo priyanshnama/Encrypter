e811d20668939ab2afea1fd45e487c71f7ecf723
akshatgondane <akshatgondane@gmail.com>
2020-10-28 21:53:01 +0530

    Commit Check Final

--- app/src/main/java/com/priyanshnama/encrypter/MainActivity.java
+++ app/src/main/java/com/priyanshnama/encrypter/MainActivity.java
@@ -55,6 +55,25 @@ public class MainActivity extends AppCompatActivity {
         setProcessedData();
         Toast.makeText(this, "decrypted", Toast.LENGTH_LONG).show();
     }
+    public void rawDataProcess(boolean encryption)
+    {
+        raw_data = getRawData();
+        if(encryption)
+        {
+            processed_data = Encrypter.encrypt(raw_data);
+            setProcessedData();
+            Toast.makeText(this, "encrypted", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            processed_data = Encrypter.decrypt(raw_data);
+            setProcessedData();
+            Toast.makeText(this, "decrypted", Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
 
     public void copy(View view) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
@@ -77,5 +96,6 @@ public class MainActivity extends AppCompatActivity {
         }
         input.setText(text);
     }
+
 
 }
